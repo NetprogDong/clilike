030483033d7d1a084e7877e779389e61f0e31157
dong <252229738@qq.com>
2020-01-19 17:41:16 +0800

    modify profile.py

--- profile.py
+++ profile.py
@@ -5,10 +5,10 @@
 # 环境参数
 env_parameter = {
                   'log_dir' : '/Users/dong/Desktop/clilikelog/',
-                  'aaa_user' : 'zhaodong3',
-                  'aaa_passwd' : 'zdnb201901',
-                  'local_user' : 'netpm',
-                  'local_passwd' : 'Netpm@s1na',
+                  'aaa_user' : 'aaa_username',
+                  'aaa_passwd' : 'aaa_userpassword',
+                  'local_user' : 'local_username',
+                  'local_passwd' : 'local_userpassowrd',
                   'enable_passwd_cisco' : 'enable_password',
                   'hostname_format_h3c' : '\r\n(<|\[)([^\n]+(-|\.|_)){3}[^\n]+(>|\])',
                   'hostname_format_cisco1' : '\r\n([^\n]+(-|\.|_)){3}[^\n]+#',
