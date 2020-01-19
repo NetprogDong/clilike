5d849686267534d9b51655fa2ccbc4992a302d90
dong <252229738@qq.com>
2020-01-19 17:34:04 +0800

    modify profile

--- profile.py
+++ profile.py
@@ -10,8 +10,8 @@ env_parameter = {
                   'local_user' : 'netpm',
                   'local_passwd' : 'Netpm@s1na',
                   'enable_passwd_cisco' : 'enable_password',
-                  'hostname_format_h3c' : '\r\n(<|\[)([^\n]+-){3}[^\n]+(>|\])|\r\n(<|\[)([^\n]+\.){3}[^\n]+(>|\])|\r\n(<|\[)([^\n]+_){3}[^\n]+(>|\])',
-                  'hostname_format_cisco1' : '\r\n([^\n]+-){3}[^\n]+#|\r\n([^\n]+\.){3}[^\n]+#|\r\n([^\n]+_){3}[^\n]+#',
-                  'hostname_format_cisco2' : '\r\n([^\n]+-){3}[^\n]+>|\r\n([^\n]+\.){3}[^\n]+>|\r\n([^\n]+_){3}[^\n]+>',
+                  'hostname_format_h3c' : '\r\n(<|\[)([^\n]+(-|\.|_)){3}[^\n]+(>|\])',
+                  'hostname_format_cisco1' : '\r\n([^\n]+(-|\.|_)){3}[^\n]+#',
+                  'hostname_format_cisco2' : '\r\n([^\n]+(-|\.|_)){3}[^\n]+>',
 
                   }
